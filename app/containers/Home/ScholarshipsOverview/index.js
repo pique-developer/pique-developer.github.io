fea286fba81604d3e3eeeea09dfa633f93c4999d
Evan Turner <evturn@gmail.com>
2016-10-28 21:50:58 -0400

    Add animation delay for Scholarships Overview on Home page

--- app/containers/Home/ScholarshipsOverview/index.js
+++ app/containers/Home/ScholarshipsOverview/index.js
@@ -5,32 +5,65 @@ import mask from './mask.png'
 import css from './style.css'
 
 export class ScholarshipsOverview extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {animate: false}
+    this.listenForScroll = ::this.listenForScroll
+    this.stopListening = ::this.stopListening
+  }
+
+  componentDidMount() {
+    this.listenForScroll()
+  }
+
+  componentWillUnmount() {
+    this.stopListening()
+  }
+
+  listenForScroll() {
+    this.listener = window.addEventListener('scroll', _ => {
+      const scrollTop = window.scrollY
+      if (scrollTop > 921 && !this.state.animate) {
+        this.setState({animate: true})
+        this.stopListening()
+      }
+    })
+  }
+
+  stopListening() {
+    window.removeEventListener('scroll', this.listener)
+  }
+
   render() {
+    const { animate } = this.state
     return (
       <div className={css.root}>
         <div className={css.mask} />
-        {halves.map((x, i) =>{
-        return <div key={i} className={css.half}>
-          <div className={i === 0 ? css.fst : css.scd}>
-            <div className={i === 0 ? css.left : css.right}>
-              <div className={css.title}>{x.title}</div>
-              <div className={css.lines}>
-                {x.lines.map((y, j) =>
-                  <div key={j} className={i === 0 ? css.line1 : css.line2}>
-                    {y.lead ? <span className={css.lead}>{y.lead} </span> : ''}{y.text}
-                  </div>
-                )}
+        {halves.map((x, i) =>
+          <div key={i} className={css.half}>
+            <div className={i === 0 ? css.fst : css.scd}>
+              <div className={i === 0 ? css.left : css.right}>
+                <div className={css.title}>{x.title}</div>
+                <div className={css.lines}>
+                  {x.lines.map((y, j) =>
+                    <div key={j} className={i === 0 ? css.line1 : css.line2}>
+                      {y.lead ? <span className={css.lead}>{y.lead} </span> : ''}{y.text}
+                    </div>
+                  )}
+                </div>
+                <div className={i === 0 ? css.btns1 : css.btns2}>
+                  <button className={css.btn} onClick={this.props.onClick}>Sign Up!</button>
+                  <button className={css.btn}>{x.button}</button>
+                </div>
               </div>
-              <div className={i === 0 ? css.btns1 : css.btns2}>
-                <button className={css.btn} onClick={this.props.onClick}>Sign Up!</button>
-                <button className={css.btn}>{x.button}</button>
-              </div>
-            </div>
-              <img className={i > 0 ? css.img1 : css.img2} src={x.image} />
+              <img className={
+                i > 0
+                  ? animate ? `${css.img1} ${css.fade1}` : css/img1
+                  : animate ? `${css.img2} ${css.fade2}` : css.img2
+              } src={x.image} />
             </div>
           </div>
-
-        })}
+        )}
       </div>
     )
   }
