6e4eae8394b5df809d481cb33d06737d111e2a57
Evan Turner <evturn@gmail.com>
2016-10-29 05:12:10 -0400

    Create join section on Students page

--- app/containers/Students/StudentsJoin/index.js
+++ app/containers/Students/StudentsJoin/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import css from './style.css'
 
 export const StudentsJoin = props => {
   return (
-    <div></div>
+    <div className={css.root}>
+      <div className={css.header}>Ready to Start Winning Scholarships?</div>
+      <div className={css.btn}>Join. It's Free.</div>
+    </div>
   )
 }
 
