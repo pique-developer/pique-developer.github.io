ec645df487be212d245aba530c14c99dcacf6fa2
Evan Turner <evturn@gmail.com>
2016-11-16 18:20:19 -0500

    Add toggle to app sidebar items

--- app/members/MembersSidebar/index.js
+++ app/members/MembersSidebar/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Link from 'react-router/Link'
+import SidebarDropdown from './SidebarDropdown'
 import student from './student.png'
-import caret from './caret.png'
 import css from './style.css'
 
 const MembersSidebar = ({ routes }) => {
@@ -19,29 +18,5 @@ const MembersSidebar = ({ routes }) => {
     </div>
   )
 }
-
-const SidebarDropdown = ({ routes, title }) => {
-  return (
-    <div>
-      <div className={css.dropdown}>
-        <div className={css.title}>{title}</div>
-        <img className={css.caret} src={caret} />
-      </div>
-      <div className={css.links}>
-        {routes.map((x, i) =>
-          <Link
-            key={i}
-            className={css.link}
-            to={x.route}
-            activeClassName={css.active}
-            activeOnlyWhenExact={x.activeOnlyWhenExact}>
-            <div className={css.caption}>{x.alias || x.caption}</div>
-          </Link>
-        )}
-      </div>
-    </div>
-  )
-}
-
 
 export default MembersSidebar
