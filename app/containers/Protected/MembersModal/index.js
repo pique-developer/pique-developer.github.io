8df3184d94039c289f979ed4a275035f3cde45dd
Evan Turner <evturn@gmail.com>
2016-12-09 19:59:48 -0500

    Add share field dynamically on invite members modal

--- app/containers/Protected/MembersModal/index.js
+++ app/containers/Protected/MembersModal/index.js
@@ -1,29 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Modal from 'components/Modal'
 import * as Actions from 'api/actions'
 import css from './style.css'
 
-export const MembersModal = ({ open, removeModal }) => {
-  return (
-    <Modal
-      open={open}
-      onClick={removeModal}
-      className={css.curtain}>
-      <div className={css.modal}>
-        <div className={css.title}>Invite members</div>
+export class MembersModal extends Component {
+  constructor(props) {
+    super(props)
+    this.onChange = ::this.onChange
+  }
 
-        <div className={css.label}>People</div>
-        <input
-          className={css.input}
-          placeholder="Enter email addresses..."
-          type="text" />
-        <div className={css.done}>
-          <button className={css.btn}>Done</button>
+  state = {share: false}
+
+  onChange(e) {
+    const { value } = e.target
+    this.setState({share: !!value.length})
+  }
+
+  render() {
+    const { share } = this.state
+    const { removeModal, open } = this.props
+    return (
+      <Modal
+        open={open}
+        onClick={removeModal}
+        className={css.curtain}>
+        <div className={css.modal}>
+          <div className={css.title}>Invite members</div>
+          <div className={css.label}>People</div>
+
+          <input
+            onChange={this.onChange}
+            className={css.input}
+            placeholder="Enter email addresses..."
+            type="text" />
+
+          <div className={`${!share ? css.hide : ''}`}>
+            <input className={css.input} type="text" />
+          </div>
+
+          <div className={css.done}>
+            <button className={css.btn}>Done</button>
+          </div>
         </div>
-      </div>
-    </Modal>
-  )
+      </Modal>
+    )
+  }
 }
 
 export default connect(
