dcd92c96a93e47055c4e490534923970f9bd3651
Evan Turner <evturn@gmail.com>
2017-01-03 02:14:27 -0500

    Remove express.static from prod server

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,19 +3,20 @@
 require('babel-polyfill');
 
 var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-
-app.use('/build', express.static('build'));
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json())
+// app.use('/build', express.static('build'))
 
 var stripeEndpoint = require('./api/stripeEndpoint');
 app.use('/api/stripe', stripeEndpoint);
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: path.join(__dirname, '../../build') });
+app.get('*', function (req, res) {
+  var filepath = path.join(process.cwd(), 'build', 'index.html');
+  res.sendFile(fs.readFileSync(filepath).toString());
 });
 
 app.listen(process.env.PORT || 5000);
--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,19 @@
 import 'babel-polyfill';
 const path = require('path');
+const fs = require('fs')
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
-
-app.use('/build', express.static('build'))
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json())
+// app.use('/build', express.static('build'))
 
 const stripeEndpoint = require('./api/stripeEndpoint')
 app.use('/api/stripe', stripeEndpoint)
 
-app.get('/', function(req, res) {
-  res.sendFile('index.html', {root: path.join(__dirname, '../../build')})
+app.get('*', (req, res) => {
+  const filepath = path.join(process.cwd(), 'build', 'index.html')
+  res.sendFile(fs.readFileSync(filepath).toString())
 })
 
 app.listen(process.env.PORT || 5000)
